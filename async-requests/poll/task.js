cbd96e3ac513c02fee3c94e3e320e91070d6d326
BOLOTOVMAX <bolotovmax1983@mail.ru>
2023-06-24 18:25:36 +0300

    first commit poll

---
+++ async-requests/poll/task.js
@@ -0,0 +1,18 @@
+const pollTitle = document.getElementById('poll__title');
+const pollAnswers = document.getElementById('poll__answers');
+
+fetch('https://students.netoservices.ru/nestjs-backend/poll')
+  .then(response => response.json())
+  .then(data => {
+    pollTitle.textContent = data.data.title;
+    data.data.answers.forEach(answer => {
+        const answerButton = document.createElement('button');
+        answerButton.classList.add('poll__answer');
+        answerButton.textContent = answer;
+        answerButton.addEventListener('click', () => {
+          alert('Спасибо, ваш голос засчитан!');
+        });
+        pollAnswers.appendChild(answerButton);
+      });
+    })
+    .catch(error => console.error(error));
