7a6e49fea22a05cc15808f2352cb702e349a54e6
BOLOTOVMAX <bolotovmax1983@mail.ru>
2023-06-28 20:03:42 +0300

    first commit auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,37 +1,60 @@
 const signinForm = document.getElementById('signin__form');
 const welcomeBlock = document.getElementById('welcome');
-const userIdSpan = document.getElementById('user_id');
+const userIdElement = document.getElementById('user_id');
 
-const userId = localStorage.getItem('user_id');
-if (userId) {
-  userIdSpan.textContent = userId;
-  welcomeBlock.classList.add('welcome_active');
-}
+// При отправке формы
+signinForm.addEventListener('submit', async (event) => {
+  event.preventDefault(); // Отменяем стандартное поведение формы
 
-signinForm.addEventListener('submit', (event) => {
-  event.preventDefault();
+  // Получаем данные из формы
+  const formData = new FormData(signinForm);
+  const login = formData.get('login');
+  const password = formData.get('password');
 
-  const login = signinForm.elements.login.value;
-  const password = signinForm.elements.password.value;
-
-  fetch('https://students.netoservices.ru/nestjs-backend/auth', {
+  // Отправляем POST-запрос на сервер
+  const response = await fetch('https://students.netoservices.ru/nestjs-backend/auth', {
     method: 'POST',
     body: JSON.stringify({ login, password }),
     headers: {
       'Content-Type': 'application/json'
     }
-  })
-    .then(response => response.json())
-    .then(data => {
-      if (data.success) {
-        localStorage.setItem('user_id', data.user_id);
-        userIdSpan.textContent = data.user_id;
-        welcomeBlock.classList.add('welcome_active');
-      } else {
-        alert('Неверный логин/пароль');
-      }
-    })
-    .catch(error => {
-      console.error('Ошибка:', error);
-    });
+  });
+
+  // Проверяем статус ответа
+  if (response.ok) {
+    const data = await response.json();
+
+    if (data.success) {
+      // Сохраняем id пользователя в локальное хранилище
+      localStorage.setItem('user_id', data.user_id);
+
+      // Отображаем блок приветствия с id пользователя
+      userIdElement.textContent = data.user_id;
+      welcomeBlock.classList.add('welcome_active');
+
+      // Скрываем блок с инпутами
+      const signinInputsBlock = document.getElementById('signin');
+      signinInputsBlock.style.display = 'none';
+    } else {
+      alert('Неверный логин/пароль');
+    }
+  } else {
+    alert('Ошибка сервера');
+  }
+});
+
+// При загрузке страницы
+window.addEventListener('load', () => {
+  // Проверяем, есть ли сохраненный id пользователя в локальном хранилище
+  const userId = localStorage.getItem('user_id');
+
+  if (userId) {
+    // Отображаем блок приветствия с сохраненным id пользователя
+    userIdElement.textContent = userId;
+    welcomeBlock.classList.add('welcome_active');
+
+    // Скрываем блок с инпутами
+    const signinInputsBlock = document.getElementById('signin');
+    signinInputsBlock.style.display = 'none';
+  }
 });
